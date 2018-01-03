eba67466836be40a4ec203dd22ea032b665797cf
haonx <qtvhao@gmail.com>
2018-01-03 23:37:36 +0700

    case correct answers selected then do action

---
+++ quiz-single-multiple-choice.js
@@ -0,0 +1,31 @@
+angular.module("SingleMultipleChoiceQuiz", []).directive("singleMultipleChoiceQuiz", function () {
+    return {
+        templateUrl: "quiz-single-multiple-choice.html",
+        scope: {
+            eventCorrect: "&",
+            answers: "="
+        },
+        link: function (scope) {
+            scope.selectAnswer = function (answer) {
+                for (var i = 0; i < scope.answers.length; i++) {
+                    if (answer.id === scope.answers[i].id) {
+                        scope.answers[i].selected = true;
+                    } else {
+                        scope.answers[i].selected = false;
+                    }
+                }
+            };
+            scope.$watch("answers|json", function (newVal) {
+                var correctAnswers = scope.answers.filter(function (value) {
+                    return value.selected && value.isCorrect;
+                });
+                var selectedAnswers = scope.answers.filter(function (value) {
+                    return value.selected;
+                });
+                if(correctAnswers.length === selectedAnswers.length) {
+                    scope.eventCorrect();
+                }
+            })
+        }
+    };
+});
